6c0e214121a0f3d2bc137954623479d5d6365df9
Felipe Layos <jfelipelayos@gmail.com>
2020-10-06 12:57:53 -0500

    Fix name typos

--- client/src/Components/Stateless/NavItem.js
+++ client/src/Components/Stateless/NavItem.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './NavItem.css';
-
+import React from "react";
+import "./Styles.css";
 
 export default function Dashboard(props) {
-    return (
-        <div className={'dashboard_' + props.className}>
-            <img className={'icon_' + props.className} src={props.icon} alt=''></img>
-            <div className={'text_' + props.className}><p>{props.text}</p></div>
-        </div>
-    );
+  return (
+    <div className={"nav_item_" + props.className}>
+      <img className={"icon_" + props.className} src={props.icon} alt=""></img>
+      <div className={"text_" + props.className}>
+        <p>{props.text}</p>
+      </div>
+    </div>
+  );
 }
