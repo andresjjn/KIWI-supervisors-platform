a747769e9a51aafc24409fa0a5dac7fa52e50336
AndersenCastaneda <andersenpiano@gmail.com>
2020-10-07 15:33:02 -0500

    Clean App.js and Destructuring NavItem

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-// import Menu from './menu_view/menu_view'
-// import Logout from './button_logout/logout_button'
-// import Notification from './button_notification/notification_button'
+import Logout from './button_logout/logout_button'
+import Notification from './button_notification/notification_button'
 import Navbar from './Components/Statefull/NavBar';
 
 export default class App extends Component {
     render() {
         return (
             <div className="App">
-                {/* <Menu/> */}
-                {/* <Logout /> */}
-                {/* <Notification /> */}
+                <Logout />
+                <Notification />
                 <Navbar/>
             </div>
         );
--- client/src/Components/Stateless/NavItem.js
+++ client/src/Components/Stateless/NavItem.js
@@ -3,10 +3,11 @@ import './NavItem.css';
 
 
 export default function Dashboard(props) {
+    const {className, icon, text} = props;
     return (
-        <div className={'dashboard_' + props.className}>
-            <img className={'icon_' + props.className} src={props.icon} alt=''></img>
-            <div className={'text_' + props.className}><p>{props.text}</p></div>
+        <div className={'dashboard_' + className}>
+            <img className={'icon_' + className} src={icon} alt=''></img>
+            <div className={'text_' + className}><p>{text}</p></div>
         </div>
     );
 }
