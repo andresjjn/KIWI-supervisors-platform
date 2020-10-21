73ed0c1a4115086633dd23e31613f85b1e5f146c
Felipe Layos <jfelipelayos@gmail.com>
2020-10-21 15:21:48 -0500

    Added user info and log out alert

--- client/src/auth/Profile.js
+++
@@ -1,8 +0,0 @@
-import { useAuth0 } from "@auth0/auth0-react";
-
-const LogoutButton = () => {
-    const user = useAuth0();
-    return user;
-};
-
-export default LogoutButton;
--- client/src/components/statefull/info_bar/InfoBar.jsx
+++ client/src/components/statefull/info_bar/InfoBar.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Logout from '../logout/Logout';
-import AuthenticationButton from '../AuthenticationButton';
-import Notification from '../notification/Notification';
+import { useAuth0 } from "@auth0/auth0-react";
+// import Notification from '../notification/Notification';
 import './InfoBar.css'
 
 export default function InfoBar() {
+    const { user } = useAuth0();
     return (
         <div className="info_bar">
-            <Logout />
-            <AuthenticationButton />
-            <Notification />
+            <div className="info_user">
+                <p>{user.name}-</p>
+                <p>{user.sub.slice(6)}</p>
+            </div>
+            <div className="info_container">
+                <Logout />
+            </div>
         </div>
     );
 }
